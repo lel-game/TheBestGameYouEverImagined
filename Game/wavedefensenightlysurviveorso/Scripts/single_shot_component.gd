e8fc57ea7dbf00059dba8fd17bfa174afed46e69
SmellyRodent <arturyevsieienko@gmail.com>
2025-04-21 16:59:46 -0400

    Updated spread code
    sohuld be more readable and uses degrees instead of radians now

--- Game/wavedefensenightlysurviveorso/Scripts/single_shot_component.gd
+++ Game/wavedefensenightlysurviveorso/Scripts/single_shot_component.gd
@@ -12,8 +12,9 @@ func shoot():
 	weapon_manager.gun_data.current_ammo -= 1
 	weapon_manager.gun_data.current_ammo = clamp(weapon_manager.gun_data.current_ammo,0,weapon_manager.gun_data.magazine_size)
 	#SWAY WEAPON
-	weapon_manager.rotate_x(randf_range(-weapon_manager.gun_data.spread,weapon_manager.gun_data.spread))
-	weapon_manager.rotate_y(randf_range(-weapon_manager.gun_data.spread,weapon_manager.gun_data.spread))
+	var sway_amount = deg_to_rad(weapon_manager.gun_data.spread)
+	weapon_manager.rotate_x(randf_range(-sway_amount,sway_amount))
+	weapon_manager.rotate_y(randf_range(-sway_amount,sway_amount))
 	#SPAWN IN BULLET
 	var new_bullet = (bullet.instantiate() as Bullet)
 	new_bullet.global_position = muzzle_position.global_position
